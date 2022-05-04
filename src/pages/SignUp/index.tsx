ae205fa7336a49e609ed0bcf1d11ec1805bda061
Augusto Gomes <augusto.tgomes@gmail.com>
2022-05-04 13:17:59 -0300

    tela de cadastro e navegação entre login e cadastro

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,59 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import {useNavigation} from '@react-navigation/native';
 
-import {Container} from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import {Container, Title, BackToSignIn, BackToSignInText} from './styles';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  const navigation = useNavigation();
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{flex: 1}}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled>
+        <ScrollView
+          contentContainerStyle={{flex: 1}}
+          keyboardShouldPersistTaps="handled">
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input name="nome" icon="user" placeholder="email" />
+            <Input name="email" icon="mail" placeholder="email" />
+            <Input name="senha" icon="lock" placeholder="senha" />
+
+            <Button
+              onPress={() => {
+                console.log('teste');
+              }}>
+              Cadastrar
+            </Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <BackToSignIn onPress={() => navigation.goBack()}>
+        <Icon name="arrow-left" size={20} color="#ff9000" />
+        <BackToSignInText>Voltar para login</BackToSignInText>
+      </BackToSignIn>
+    </>
+  );
 };
 
 export default SignUp;
